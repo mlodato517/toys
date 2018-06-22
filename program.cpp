9b9adf8d67a3896155ab31d9fe66ac660071a843
mlodato517 <mlodato517@gmail.com>
2018-06-22 17:55:40 -0400

    Add python and gitignore

---
+++ program.cpp
@@ -0,0 +1,181 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <time.h>
+using namespace std;
+
+// Returns char representation of coin value
+// value - value of coin to get char of
+char getCoinName(int value) {
+  switch (value) {
+    case 1: return 'p';
+    case 5: return 'n';
+    case 10: return 'd';
+    case 25: return 'q';
+    default: return '?';
+  }
+}
+
+// Gets int arrays corresponding to valid coin sequences
+// numHours             - number of hours on the clock
+// values               - coin values we have (must match with counts)
+// counts               - count of each coin value (must match with values)
+// numValues            - number of different coin values we have
+// currentSequence      - array of coin values we've placed
+// clockState           - for each clock hour, true if a coin is on it
+// currentValue         - current clock hour we're on
+// currentSequenceIndex - index of currentSequence we're on
+vector<int*> getValidSequences(
+  int numHours,
+  int *values,
+  int *counts,
+  int numValues,
+  int *currentSequence,
+  bool *clockState,
+  int currentValue,
+  int currentSequenceIndex
+) {
+  vector<int*> returnValues;
+
+  // If we're on the last hour, we have a valid sequence
+  if (currentSequenceIndex == numHours) {
+
+    // Copy the array and push it onto the vector
+    int *copiedSequence = new int[numHours];
+    for (int i = 0; i < numHours; i += 1) {
+      *(copiedSequence + i) = *(currentSequence + i);
+    }
+    returnValues.push_back(copiedSequence);
+  }
+  else {
+
+    // FOr each coin value...
+    for (int i = 0; i < numValues; i += 1) {
+
+      // If we've run out of this coin value, continue
+      if (*(counts + i) == 0) continue;
+
+      // See where the next coin would go
+      int value = *(values + i);
+      int nextValue = (currentValue + value) % numHours;
+
+      // If there's already a coin there, continue
+      if (*(clockState + nextValue)) continue;
+
+      // Place the coin
+      *(currentSequence + currentSequenceIndex) = value;
+      currentSequenceIndex += 1;
+      *(clockState + nextValue) = true;
+      *(counts + i) -= 1;
+
+      // Recurse
+      vector<int*> recursedValues = getValidSequences(
+        numHours,
+        values,
+        counts,
+        numValues,
+        currentSequence,
+        clockState,
+        nextValue,
+        currentSequenceIndex
+      );
+
+      // Concatenate vectors
+      returnValues.insert(returnValues.end(), recursedValues.begin(), recursedValues.end());
+
+      // Remove coin
+      currentSequenceIndex -= 1;
+      *(currentSequence + currentSequenceIndex) = 0;
+      *(clockState + nextValue) = false;
+      *(counts + i) += 1;
+    }
+
+    return returnValues;
+  }
+}
+
+// Gets vector of strings of valid coin sequences
+// numHours  - number of hours on the clock
+// values    - coin values we have (must match with counts)
+// counts    - count of each coin value (must match with values)
+// numValues - number of different coin values we have
+vector<string> getValidSequences(
+  int numHours,
+  int *values,
+  int *counts,
+  int numValues
+) {
+
+  // Would a packed bit vector peform better?
+  // Initialize clock state to all false (there are no coins after all!)
+  bool *clockState = new bool[numHours];
+  for (int i = 0; i < numHours; i += 1) {
+    *(clockState + i) = false;
+  }
+
+  // Initialize currentSequence to all 0s so we can
+  // I'm not actually sure I need to do this...
+  int* currentSequence  = new int[numHours];
+  for (int i = 0; i < numHours; i += 1) {
+    *(currentSequence + i) = 0;
+  }
+
+  vector<int*> sequences = getValidSequences(
+    numHours,
+    values,
+    counts,
+    numValues,
+    currentSequence,
+    clockState,
+    0,
+    0
+  );
+
+  delete[] clockState;
+  delete[] currentSequence;
+
+  // Convert to char vector
+  vector<string> results (sequences.size());
+  for(int i = 0; i < sequences.size(); i += 1) {
+    int *sequence = sequences[i];
+    char *result = new char[numHours + 1];
+    for (int j = 0; j < numHours; j += 1) {
+      *(result + j) = getCoinName(*(sequence + j));
+    }
+    *(result + numHours) = '\0';
+
+    string stringResult = result;
+    results[i] = stringResult;
+    delete[] result;
+    delete[] sequence;
+  }
+
+  return results;
+}
+
+int main() {
+  int numHours= 12;
+  int coins[] = {1, 5, 10};
+  int counts[] = {4, 4, 4};
+  int numValues = 3;
+
+  vector<string> values;
+
+  // Get the number of ticks it takes for 1000 calculations.
+  clock_t t;
+  t = clock();
+  for (int i = 0; i < 1000; i += 1) {
+    values = getValidSequences(numHours, &coins[0], &counts[0], numValues);
+  }
+  t = clock() - t;
+
+  // Multiple by 1,000,000 to get ticks for 1e9 calculations.
+  // Divide by ticks/sec to get ns/calculation.
+  cout << "Time: " << ((double)t) * 1000 * 1000 / CLOCKS_PER_SEC << "ns" << endl;
+
+  for(int i = 0; i < values.size(); i += 1) {
+    cout << values[i] << endl; // Got ~210,000 ns
+  }
+
+  getchar();
+}
---
+++ python/program.py
@@ -0,0 +1,118 @@
+from collections import deque
+import time
+
+'''
+_GetValidSequences
+Gets list of list of ints
+Each list of ints represents a valid coin sequence on the clock
+numHours        - Int. Number of hours on the clock
+coins           - Array of ints. Must be matched with counts. Gives value of each coin
+counts          - Array of ints. Must be matched with coins. Gives count of each coin
+clockState      - Array of bools. Lists which clock hours have coins
+currentSequence - List of ints. Current sequence of coins placed
+currentValue    - Int. Current value of clock we're on
+'''
+def _GetValidSequences(
+  numHours,
+  coins,
+  counts,
+  clockState,
+  currentSequence,
+  currentValue
+):
+  returnValues = []
+
+  # If we have numHours coins in our sequence, we've
+  # found a solution. Add it to returnValues.
+  if (len(currentSequence) == numHours):
+    returnValues.append(currentSequence[:])
+  else:
+    for i in range(len(coins)):
+
+      # If we ran out of this coin, continue
+      if counts[i] == 0:
+        continue
+
+      # Determine where the next coin would go
+      coinValue = coins[i]
+      nextValue = (currentValue + coinValue) % numHours
+
+      # If there's already a coin there, continue
+      if clockState[nextValue]:
+        continue
+
+      # Place the coin on the clock
+      currentSequence.append(coinValue)
+      clockState[nextValue] = True
+      counts[i] -= 1
+
+      # Recurse
+      returnValues.extend(_GetValidSequences(
+        numHours,
+        coins,
+        counts,
+        clockState,
+        currentSequence,
+        nextValue
+      ))
+
+      # Remove the coin from the clock to try the next coin
+      currentSequence.pop()
+      clockState[nextValue] = False
+      counts[i] += 1
+
+  return returnValues
+
+'''
+GetCoinName
+Returns character that is the name of the coin based on the given value
+value - value of coin to get character of
+'''
+def GetCoinName(value):
+  if value == 1: return 'p'
+  if value == 5: return 'n'
+  if value == 10: return 'd'
+  return '?'
+
+'''
+GetValidSequences
+Gets list of strings representing valid clock coin sequences
+numHours        - Int. Number of hours on the clock
+coins           - Array of ints. Must be matched with counts. Gives value of each coin
+counts          - Array of ints. Must be matched with coins. Gives count of each coin
+'''
+def GetValidSequences(numHours, coins, counts):
+  clockState = [False] * numHours
+  currentSequence = []
+
+  sequences = _GetValidSequences(
+    numHours,
+    coins,
+    counts,
+    clockState,
+    currentSequence,
+    0
+  )
+
+  # Convert list of list of ints to list of strings
+  # The list generator takes the list of ints and generates
+  # a list of coin names. ''.join() joins those into a single string
+  for i in range(len(sequences)):
+    sequences[i] = ''.join([GetCoinName(coinValue) for coinValue in sequences[i]])
+
+  return sequences
+
+
+def main ():
+  numHours = 12
+  coins = [1, 5, 10]
+  counts = [4, 4, 4]
+  print(f'Number of hours = {numHours}\nCoins = {coins}\nCounts = {counts}')
+
+  start = time.time()
+  sequences = GetValidSequences(numHours, coins, counts)
+  end = 1000 * 1000 * 1000 * (time.time() - start)
+  print(f'{end} ns') # 177,058,458 ns
+  print(sequences)
+
+main()
